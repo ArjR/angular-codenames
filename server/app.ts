9c6e70c22ed61aaef96b2fa0c40f99263aead65f
Unknown <arjun.roy@me.com>
2018-01-14 15:37:47 +0000

    Static Files Fix

--- server/app.ts
+++ server/app.ts
@@ -32,13 +32,14 @@ if (app.get("env") === "production") {
 
   // in production mode run application from dist/client folder
   app.use(express.static(path.join(__dirname, "/../client")));
-  app.use('/data', express.static(__dirname + '/../server/data'));
+  app.use('/data', express.static(path.join(__dirname, "/../server/data")));
 }
+else {
+  //app.use(express.static(__dirname));
 
-//app.use(express.static(__dirname));
-
-// Serve static .txt files only inside the /data/ directory
-app.use('/data', express.static(__dirname + '/data'));
+  // Serve static .txt files only inside the /data/ directory
+  app.use('/data', express.static(__dirname + '/data'));
+}
 
 // catch 404 and forward to error handler
 app.use((req: express.Request, res: express.Response, next) => {
